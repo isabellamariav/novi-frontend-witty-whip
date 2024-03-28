9047bd9e18abf03b21e506ade911dc25b73f3ff3
isabellamariav <isabella.verhoeven@gmail.com>
2024-03-28 13:11:35 +0100

    Navagation layout and added assets and flavicon.

--- src/constants/navigation/Navigation.jsx
+++ src/constants/navigation/Navigation.jsx
@@ -1,42 +1,46 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import NavButton from '../navButton/NavButton';
+import logo from '../../assets/logo.jpg';
 import './Navigation.css';
 
 const Navigation = () => {
     return (
         <div>
-            {/* Bovenste navigatiebalk */}
             <div className="top-navbar">
-                <div className="logo">Logo</div>
-                <div className="website-name">Witty Whip</div>
-                <div className="sign-in-button">
-                    <Link to="/signin">
-                        <NavButton text="Sign In" />
-                    </Link>
+                <div className="left-section">
+                    <div className="logo">
+                        <img src={logo} alt="Logo" />
+                    </div>
+                </div>
+                <div className="center-section">
+                    <div className="website-name">Witty Whip</div>
+                </div>
+                <div className="right-section">
+                    <div className="sign-in-button">
+                        <Link to="/signin">
+                            <NavButton text="Sign In" />
+                        </Link>
+                    </div>
                 </div>
             </div>
 
-            {/* Onderste navigatiebalk */}
             <div className="bottom-navbar">
-                <Link to="/about">
-                    <NavButton text="About" />
-                </Link>
-                <Link to="/all-recipes">
-                    <NavButton text="All Recipes" />
-                </Link>
-                <Link to="/home">
-                    <NavButton text="Home" />
-                </Link>
-                <Link to="/profile">
-                    <NavButton text="Profile" />
-                </Link>
-                <Link to="/questionnaire">
-                    <NavButton text="Questionnaire" />
-                </Link>
+                <NavItem to="/home" text="Home" />
+                <NavItem to="/all-recipes" text="All Recipes" />
+                <NavItem to="/questionnaire" text="Questionnaire" />
+                <NavItem to="/profile" text="Profile" />
+                <NavItem to="/about" text="About" />
             </div>
         </div>
     );
-}
+};
+
+const NavItem = ({ to, text }) => {
+    return (
+        <Link to={to}>
+            <NavButton text={text} />
+        </Link>
+    );
+};
 
 export default Navigation;
